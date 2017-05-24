35070d21808b9c0273d5be945e1028005d3b5b1e
FRVisser <fokkovisser@gmail.com>
2017-05-24 21:57:40 +0200

    change image and spread 100%

--- overlay.js
+++ overlay.js
@@ -14,7 +14,7 @@ div.style.margin = "0";
 div.style.top = "0";
 div.style.left = "0";
 //hier de inhoud van de div
- div.innerHTML = "<img src='https://scontent-ams3-1.xx.fbcdn.net/v/t1.0-9/18671079_1532982446746113_366739055548157275_n.jpg?oh=3c6d8358379e4ee849dfd183cee06c14&oe=59A29C37'/>";
+ div.innerHTML = "<img src='https://frvisser.github.io/Hilde_Overlay/img/mask-2.png' style='width: 100%;'>";
  div.style.display = "none";
 
 
