6cdb3b7020ebf44861c5a1b0ce9fff64c592f24f
FRVisser <fokkovisser@gmail.com>
2017-05-24 22:04:57 +0200

    up and down, + full with and height

--- overlay.js
+++ overlay.js
@@ -7,14 +7,14 @@ div.id = "showdiv";
 //css styling
 div.style.width = "100vw";
 div.style.height = "100vh";
-div.style.background = "blue";
+// div.style.background = "blue";
 div.style.position = "fixed";
 div.style.zIndex = "999";
 div.style.margin = "0";
 div.style.top = "0";
 div.style.left = "0";
 //hier de inhoud van de div
- div.innerHTML = "<img src='https://frvisser.github.io/Hilde_Overlay/img/mask-2.png' style='width: 100%;'>";
+ div.innerHTML = "<img src='https://frvisser.github.io/Hilde_Overlay/img/mask-2.png' style='height: 100%; width: 100%;'>";
  div.style.display = "none";
 
 
@@ -30,12 +30,12 @@ function checkKey(e) {
 
     e = e || window.event;
 
-    if (e.keyCode == '38') {
+    if (e.keyCode == '38' | e.keyCode == '40') {
         //maak een nieuwe div
 
 
 
-                $('#showdiv').fadeIn(100).fadeOut(100);
+                $('#showdiv').fadeIn(80).fadeOut(80);
 
                 $('*').on('click', function(){
                   // $('#underlay').stop(true, true).show().fadeOut(100);
